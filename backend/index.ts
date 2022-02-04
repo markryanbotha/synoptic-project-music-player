f940756e769ec0be83331df1cbc6b48e0fa543ac
Mark Botha <bothamar@amazon.com>
2022-02-04 00:05:19 +0000

    Setup Port Environement Variable

--- backend/index.ts
+++ backend/index.ts
@@ -29,7 +29,7 @@ const startServer = async () => {
 
     server.applyMiddleware({ app })
 
-    app.listen({ port: 4000 }, () =>
+    app.listen({ port: process.env.PORT || 4000 }, () =>
         console.log(
             `🚀 Server ready at http://localhost:4000${server.graphqlPath}`,
         ),
